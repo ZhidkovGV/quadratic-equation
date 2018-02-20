8a292911c7a7cc0dbed1ba3592429738375a4aaf
root <zhidkob.gleb@gmail.com>
2018-02-21 01:08:40 +0300

    rdy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let solution =  {
+        a: equation.replace(/\s/g, '').match(/[^\^]\d+/g)[0],
+        b: equation.replace(/\s/g, '').match(/[^\^]\d+/g)[1],
+        c: equation.replace(/\s/g, '').match(/[^\^]\d+/g)[2],
+        answer: [0, 0]
+    }
+    solution.answer[0] = Math.round(( (-1) * solution.b + Math.sqrt( solution.b * solution.b - 4 * solution.a * solution.c)) / (2 * solution.a))
+    solution.answer[1] = Math.round(( (-1) * solution.b - Math.sqrt( solution.b * solution.b - 4 * solution.a * solution.c)) / (2 * solution.a))
+    solution.answer.sort((right, left) => {return right - left})
+    return solution.answer
 }
